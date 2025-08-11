01b9872d2af573e093b2315f899f4aeeed9f4920
sangsoo <wsckm1@gmail.com>
2025-08-11 22:47:56 +0900

    refactor: footer export name 수정

--- src/widgets/ui/layout/footer/index.tsx
+++ src/widgets/ui/layout/footer/index.tsx
@@ -6,7 +6,7 @@
  */
 import React from 'react';
 
-const Index: React.FC = () => {
+const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-100 text-gray-600 py-4 mt-8">
       <div className="container mx-auto text-center">
@@ -16,4 +16,4 @@ const Index: React.FC = () => {
   );
 };
 
-export default Index;
+export default Footer;
